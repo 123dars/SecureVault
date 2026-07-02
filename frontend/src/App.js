38fb44914eb955dbd3b054f61734b420f9588eba
Darshan B <135603566+123dars@users.noreply.github.com>
2026-07-02 15:57:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, AuthContext } from './AuthContext';
+import { ThemeProvider } from './ThemeContext';
 import { Toaster } from 'react-hot-toast'; 
 // Import Pages
 import Login from './pages/Login';
@@ -19,50 +20,52 @@ const ProtectedRoute = ({ children }) => {
 };
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Toaster position="bottom-right" reverseOrder={false} />
-        
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} /> 
-          <Route 
-            path="/mfa-setup" 
-            element={
-              <ProtectedRoute>
-                <MfaSetup />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/vault" 
-            element={
-              <ProtectedRoute>
-                <Vault />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/notes" 
-            element={
-              <ProtectedRoute>
-                <Notes />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/settings" 
-            element={
-              <ProtectedRoute>
-                <Settings />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="*" element={<Navigate to="/vault" />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <Router>
+          <Toaster position="bottom-right" reverseOrder={false} />
+          
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} /> 
+            <Route 
+              path="/mfa-setup" 
+              element={
+                <ProtectedRoute>
+                  <MfaSetup />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/vault" 
+              element={
+                <ProtectedRoute>
+                  <Vault />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/notes" 
+              element={
+                <ProtectedRoute>
+                  <Notes />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/settings" 
+              element={
+                <ProtectedRoute>
+                  <Settings />
+                </ProtectedRoute>
+              } 
+            />
+            <Route path="*" element={<Navigate to="/vault" />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 export default App;
